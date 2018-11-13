d5d95b37a318b35e87f295314aaa4d9a0b0a02d3
Karel Wintersky <karel.wintersky@yandex.ru>
2018-11-13 08:18:44 +0300

    added `hook_priority` config mechanics

--- classes/hooks/HookCopyright.class.php
+++ classes/hooks/HookCopyright.class.php
@@ -9,11 +9,24 @@
 
 class PluginBlacklist_HookCopyright extends Hook
 {
+    const ConfigKey = 'blacklist';
+    const HooksArray = [
+    ];
+
     /**
      *
      */
     public function RegisterHook()
     {
+        $plugin_config_key = $this::ConfigKey;
+        foreach ($this::HooksArray as $hook => $callback) {
+            $this->AddHook(
+                $hook,
+                $callback,
+                __CLASS__,
+                Config::Get("plugin.{$plugin_config_key}.hook_priority.{$hook}") ?? 1
+            );
+        }
     }
 
 }
