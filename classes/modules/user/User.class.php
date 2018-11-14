a28ba2c3576c9cb3d1293a8386f8fd0f8366dd07
Karel Wintersky <karel.wintersky@yandex.ru>
2018-11-14 09:42:19 +0300

    plugin BlackList fixes

--- classes/modules/user/User.class.php
+++ classes/modules/user/User.class.php
@@ -22,7 +22,7 @@ class PluginBlacklist_ModuleUser extends PluginBlacklist_Inherit_ModuleUser
         if (
             !Config::Get('plugin.blacklist.check_authorization') ||
             ($oUser && $oUser->isAdministrator()) ||
-            ($oUser && !$this->PluginBlacklist_ModuleBlacklist_blackMail($oUser->getMail(), $oUser->getLogin()))
+            ($oUser && !$this->PluginBlacklist_ModuleBlacklist_checkCredentialsBlocked($oUser->getMail(), $oUser->getLogin()))
         ) {
             return parent::Authorization($oUser, $bRemember, $sKey);
         }
